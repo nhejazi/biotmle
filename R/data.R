cb5c71ee2d3e3153e893b6fa85d68eccc3e94b12
Nima Hejazi <nima.hejazi7@gmail.com>
2016-10-22 13:08:38 -0700

    add illumina2007 Rdata

---
+++ R/data.R
@@ -0,0 +1,10 @@
+#' Baseline covariates and microarray data from 2007 study...
+#'
+#' A dataset containing various baseline covariates and microarray expression
+#' measures from Illumina arrays used in a 2007 study.
+#'
+#' @format A data frame with 144 rows and 22196 variables:
+#' \describe{
+#'   ...some information ought to go here...
+#' }
+"illumina2007"
