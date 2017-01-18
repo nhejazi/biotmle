2c624813a2fa43f554dc5f96658721863b627492
Nima Hejazi <nh@nimahejazi.org>
2017-01-18 15:04:39 -0800

    fix data-gen script, add illuminaData

--- data-raw/illuminaData.R
+++ data-raw/illuminaData.R
@@ -15,4 +15,4 @@ illuminaData <- illumina2007 %>%
   dplyr::select(which(colnames(.) %ni% c("newbenz", "current_smoking")))
 
 save(illuminaData, file = paste0(normalizePath(here("..", "data")),
-                                 "illuminaData.rda"))
+                                 "/illuminaData.rda"))
