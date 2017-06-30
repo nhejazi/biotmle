a97121ba41f9258177d6da003fdf67e961e44c0d
Nima Hejazi <nh@nimahejazi.org>
2017-06-30 11:09:30 -0700

    manuall add github ref, minor tweaks

--- coverage.R
+++ coverage.R
@@ -2,3 +2,4 @@ library(covr)
 cov <- package_coverage(type = "all", combine_types = FALSE,
                         line_exclusions = list("R/plots.R"),
                         function_exclusions = "\\.onLoad")
+cov
